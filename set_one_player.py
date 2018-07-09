8d4eebf3bb49ff8e5adfc37a78baf7379d802a75
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-09 17:43:02 +0900

    revise setps

--- set_one_player.py
+++ set_one_player.py
@@ -2,16 +2,15 @@ from socket import *
 
 
 class Player0():
-    HOSTNAME = "localhost"
-    PORT = 6000
-    ADDRESS = gethostbyname(HOSTNAME)
-
     def __init__(self):
         self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.HOSTNAME = "localhost"
+        self.PORT = 6000
+        self.ADDRESS = gethostbyname(self.HOSTNAME)
 
     def send(self, command):
         to_byte_command = command.encode(encoding='utf_8')
-        self.socket.sendto(to_byte_command, (ADDRESS, PORT))
+        self.socket.sendto(to_byte_command, (self.ADDRESS, self.PORT))
 
     def receive(self):
         data, addr = self.socket.recvfrom(4096)
@@ -19,7 +18,7 @@ class Player0():
 
 
 if __name__ == "__main__":
-    player = Player0()
+    player = Player()
     command = "(init teamKazu (goalie))"
     player.send(command)
     print("送信：", command)
