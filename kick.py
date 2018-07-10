6a872a7d7593f91035cc44c9e158e0eab4be53d1
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-10 15:24:19 +0900

    add kick.py

--- kick.py
+++ kick.py
@@ -48,7 +48,7 @@ class Player5(face_ball.Player4, threading.Thread):
                     command = "(dash 80)"
 
             else:
-                command = "(turn " + str(ballDir) + ")"
+                command = "(dash 80)"
             self.send(command)
 
     def checkNearest(self, message, ballDist, ballDir):
