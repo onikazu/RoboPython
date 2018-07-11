6d9ced614aaac4db6c2043f5e90d848b1a4cd10f
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-11 15:56:44 +0900

    add player9

--- player9.py
+++ player9.py
@@ -54,15 +54,31 @@ class Player9(map_recog.Player8, threading.Thread):
                 m_dDefenceY = ballY / 2.0 + offsetY
 
     def analyzeVisualMessage(self, message=None):
-        super().analyzeVisualMessage(message)
-        if message.find("(ball)") == -1:
-            return
-        ballDist = self.getParam(message, "(ball)", 1)
-        ballDir = self.getParam(message, "(ball)", 2)
-        rad = math.radians(self.normalizeAngle(self.m_dNeck + ballDir))
-        self.m_dBallX = self.m_dX + ballDist * math.cos(rad)
-        self.m_dBallY = self.m_dY + ballDist * math.sin(rad)
-        self.setDefencePosition(self.m_dBallX, self.m_dBallY)
+        if message is None:
+            pass
+        else:
+            OUT_OF_RANGE = 999.0
+            time = int(self.getParam(message, "see", 1))
+            if time < 1:
+                return
+            self.m_dNeck = self.getNeckDir(message)
+            if self.m_dNeck == OUT_OF_RANGE:
+                return
+            if self.checkInitialMode():
+                self.m_dX = self.m_dKickOffX
+                self.m_dY = self.m_dKickOffY
+
+            pos = self.estimatePosition(message, self.m_dNeck, self.m_dX, self.m_dY)
+            self.m_dX = pos["x"]
+            self.m_dY = pos["y"]
+            if message.find("(ball)") == -1:
+                return
+            ballDist = self.getParam(message, "(ball)", 1)
+            ballDir = self.getParam(message, "(ball)", 2)
+            rad = math.radians(self.normalizeAngle(self.m_dNeck + ballDir))
+            self.m_dBallX = self.m_dX + ballDist * math.cos(rad)
+            self.m_dBallY = self.m_dY + ballDist * math.sin(rad)
+            self.setDefencePosition(self.m_dBallX, self.m_dBallY)
 
 
 if __name__ == "__main__":
