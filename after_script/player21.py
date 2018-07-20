83b5da267291d81dbf403effef9049a117faf13c
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-20 18:38:30 +0900

    add 21

---
+++ after_script/player21.py
@@ -0,0 +1,90 @@
+import player20
+import threading
+from socket import *
+import math
+
+
+class Player21(player20.Player20, threading.Thread):
+    def __init__(self):
+        super(Player21, self).__init__()
+        self.m_debugLv21 = False
+
+    def kick_0(self):
+        t = self.m_iTime
+        kickDir = self.getDirection(self.m_dX, self.m_dY, self.m_dKickX, self.m_dKickY)
+        kickAngle = self.normalizeAngle(kickDir - self.m_dBody[t])
+        self.m_strCommand[t] = "(kick 100 {0:.4f})".format(kickAngle)
+
+    def move_0(self):
+        t = self.m_iTime
+        moveDir = self.getDirection(self.m_dX, self.m_dY, self.m_dMoveX, self.m_dMoveY)
+        moveDist = self.getDistance(self.m_dX, self.m_dY, self.m_dMoveX, self.m_dMoveY)
+        kickable_area = self.player_size + self.ball_size + self.kickable_margin
+        if moveDist < kickable_area:
+            self.m_strCommand[t] = "(turn 0)"
+            return
+        turn = self.normalizeAngle(moveDir - self.m_dBody[t])
+        speed = math.sqrt(self.m_dVX[t] * self.m_dVX[t] + self.m_dVY[t] * self.m_dVY[t])
+        turn_moment = turn * (1 + self.inertia_moment * speed * self.player_decay)
+        dist = self.getDistance(self.m_dX, self.m_dY, self.m_dMoveX, self.m_dMoveY)
+        dash_power_max = 100.0
+        dash_power = 40.0
+
+        if self.checkNearest_2(self.m_dMoveX[t], self.m_dMoveY[t]):
+            dash_power = max(40.0, self.m_dStamina[t] / self.stamina_max * 100.0)
+
+        d = dist
+
+        count = 0
+
+        while d > kickable_area and count < 50:
+            speed = speed * self.player_decay + dash_power * self.m_dEffort[self.m_iTime] * self.dash_power_rate
+            d -= speed
+            count += 1
+
+        if abs(turn) < 20.0:
+            turn = 0.0
+            dist = self.getDistance(self.m_dX, self.m_dY, self.m_dMoveX, self.m_dMoveY)
+            if dist > 0.75:
+                self.m_strCommand[t] = "(dash {0:.4f})".format(dash_power)
+        elif abs(turn) > 160.0 and dist < 3.51:
+            turn = 0.0
+            if dist > 0.75:
+                self.m_strCommand[t] = "(dash {0:.4f})".format(-dash_power)
+        elif abs(turn_moment <= 180.0):
+            self.m_strCommand[t] = "(turn {0:.4f}".format(turn_moment)
+            if self.m_debugLv21:
+                print("t = {}".format(t))
+                print("m_dMoveX[t]:{0:.4f}".format(self.m_dMoveX[t]))
+                print("m_dMoveY[t]:{0:.4f}".format(self.m_dMoveY[t]))
+                print("moveDir{0:.4f}".format(moveDir))
+                print("moveDist{0:.4f}".format(moveDist))
+                print("m_headAngle[t]{0:.4f}".format(self.m_dHeadAngle[t]))
+                print("m_dNeck[t]{0:.4f}".format(self.m_dNeck[t]))
+                print("m_dBody[t]{0:.4f}".format(self.m_dBody[t]))
+                print("speed{0:.4f}".format(speed))
+                print("turn{0:.4f}".format(turn))
+                print("turn_moment{0:.4f}".format(turn_moment))
+        else:
+            speed_dir = self.getDirection(0, 0, self.m_dVX[t], self.m_dVY[t])
+            rate = self.dash_power_rate * self.m_dEffort[t]
+            if abs(self.normalizeAngle(speed_dir - moveDir)) < 20.0:
+                dash_power = max(speed * self.player_decay)
+            else:
+                dash_power = min(-speed * self.player_decay / rate, -dash_power)
+            self.m_strCommand[t] = "(dash {})(say {})".format(dash_power, turn_moment)
+
+    def playWithBall(self):
+        t = self.m_iTime
+        super().playWithBall()
+        if self.m_debugLv21:
+            print("時刻{}".format(t))
+            print("視覚{}".format(self.m_iVisualTime))
+            print("player{}".format(self.m_iNumber))
+            print("位置{0:.4f}, {0:.4f}".format(self.m_dX[t], self.m_dY[t]))
+            print("速度{0:.4f}, {0:.4f}".format(self.m_dVX[t], self.m_dVY[t]))
+            print("首{0:.4f}".format(self.m_dNeck[t]))
+            print("体{0:.4f}".format(self.m_dBody[t]))
+            print("ball({0:.4f}, {0:.4f}".format(self.m_dBallX[t], self.m_dBallY[t]))
+            print("速度{0:.4f}, {0:.4f}".format(self.m_dBallVX[t], self.m_dBallVY[t]))
+            print("コマンド{}".format(self.m_strCommand[t]))
