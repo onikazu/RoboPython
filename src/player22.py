9942a47917aef7ba7846d89c9c85b0dca9c21070
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-23 16:22:17 +0900

    fix 22

--- src/player22.py
+++ src/player22.py
@@ -16,7 +16,7 @@ class Player22(player21.Player21, threading.Thread):
         while index0 > -1:
             index1 = message.find(")", index0 + 2)
             index2 = message.find(")", index1 + 1)
-            strObject = message[index0:index2+1]
+            strObject = message[index0:index2 + 1]
             player = self.getObjectMessage_1(strObject)
             list.append(player)
             index0.find(keyword, index2)
@@ -33,11 +33,11 @@ class Player22(player21.Player21, threading.Thread):
         index0 = obj.find(") ")
         index1 = obj.find(")", index0 + 1)
         result = ""
-        name = obj[0:index0+2]
+        name = obj[0:index0 + 2]
         index2 = name.find("\"")
         index3 = name.find("\"", index2 + 1)
         index4 = name.find(")")
-        s = "((p \"" + self.m_strTeamName + "\""
+        s = "((player \"" + self.m_strTeamName + "\""
         team = ""
         if name.startswith(s):
             team = "friend"
@@ -45,11 +45,11 @@ class Player22(player21.Player21, threading.Thread):
             team = "enemy"
         number = 0
         if index3 + 1 < index4 and name.find("(player)") == -1 and name.find("(Player)"):
-            str = name[index3+1:index4]
+            str = name[index3 + 1:index4]
             if str.find("goalie") > 0:
                 str = str.replace("goalie", " ", 1)
             number = int(float(str))
-        str = obj[index0+1:index1]
+        str = obj[index0 + 1:index1]
         st = str.split(" ")
         count = len(st)
         if count <= 0:
@@ -144,7 +144,7 @@ class Player22(player21.Player21, threading.Thread):
             player = str(self.m_listPlayer[i])
             x = self.getParam(player, "x", 1)
             y = self.getParam(player, "y", 1)
-            if d >self.getDistance(targetX, targetY, x, y):
+            if d > self.getDistance(targetX, targetY, x, y):
                 if player.find(s) > -1:
                     result = False
 
