036a0a7f330e05586f770c6e190daa2f19e3a0a4
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-27 12:42:39 +0900

    fix -10

--- src/player16.py
+++ src/player16.py
@@ -22,14 +22,11 @@ class Player16(player15.Player15, threading.Thread):
 
         # 仕様変更による改善
         speed = self.getParam(message, "speed", 1)
-        # if speed == 0:
-        #     return
-
-        # ここでエラー値999.9(out of range)が入ってしまう（getparam の引数が存在しないから）
         speed_angle = self.getParam(message, "speed", 2)
         print("type of speed angle: ", type(speed_angle))
         print("type of self.m_dNeck[self.m_iTime]: ", type(self.m_dNeck[self.m_iTime]))
         print("type of self.normalizeAngle(self.m_dNeck[self.m_iTime] + speed_angle): ", type(self.normalizeAngle(self.m_dNeck[self.m_iTime] + speed_angle)))
+        print("self.m_dNeck[self.m_iTime] + speed_angle", self.m_dNeck[self.m_iTime] + speed_angle)
         rad = self.normalizeAngle(self.m_dNeck[self.m_iTime] + speed_angle) * math.pi / 180.0
         vx = speed * math.cos(rad)
         vy = speed * math.sin(rad)
