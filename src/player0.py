0b6f7a5193d9b7aadb69568d797ef7ede72699ac
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-30 14:28:22 +0900

    change tcpip

--- src/player0.py
+++ src/player0.py
@@ -1,6 +1,42 @@
 from socket import *
 
 
+# # UDPによる通信
+# class Player0():
+#     def __init__(self):
+#         # UDPはSOCK_DGRAM
+#         # TCPIPはSOCK_STREAM
+#         self.socket = socket(AF_INET, SOCK_DGRAM)
+#         # self.socket = socket(AF_INET, SOCK_STREAM)
+#         self.HOSTNAME = "localhost"
+#         self.PORT = 6000
+#         self.ADDRESS = gethostbyname(self.HOSTNAME)
+#
+#     def send(self, command):
+#         if len(command) == 0:
+#             return
+#         to_byte_command = command.encode(encoding='UTF-8')
+#         self.socket.sendto(to_byte_command, (self.ADDRESS, self.PORT))
+#
+#     def receive(self):
+#         message, arr = self.socket.recvfrom(4096)
+#         message = message.decode("UTF-8")
+#         return message
+#         # print(data)
+#
+#
+# if __name__ == "__main__":
+#     player = Player0()
+#     command = "(init teamKazu (goalie))"
+#     player.send(command)
+#     print("send:", command)
+#     message = player.receive()
+#     print("receive:", message)
+#     print("completed")
+#     while True:
+#         message = player.receive()
+
+# TCP/IP による通信
 class Player0():
     def __init__(self):
         # UDPはSOCK_DGRAM
@@ -10,15 +46,16 @@ class Player0():
         self.HOSTNAME = "localhost"
         self.PORT = 6000
         self.ADDRESS = gethostbyname(self.HOSTNAME)
+        self.socket.connect((self.ADDRESS, self.HOSTNAME))
 
     def send(self, command):
         if len(command) == 0:
             return
         to_byte_command = command.encode(encoding='UTF-8')
-        self.socket.sendto(to_byte_command, (self.ADDRESS, self.PORT))
+        self.socket.sendall(to_byte_command)
 
     def receive(self):
-        message, arr = self.socket.recvfrom(4096)
+        message = self.socket.recv(1024)
         message = message.decode("UTF-8")
         return message
         # print(data)
