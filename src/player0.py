a4eb566714f78405598beb89fa81d82b70c5dd5c
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-07-31 14:51:02 +0900

    fix

--- src/player0.py
+++ src/player0.py
@@ -1,65 +1,29 @@
 from socket import *
-
-
-# UDPによる通信
-class Player0():
-    def __init__(self):
-        # UDPはSOCK_DGRAM
-        # TCPIPはSOCK_STREAM
-        self.socket = socket(AF_INET, SOCK_DGRAM)
-        # self.socket = socket(AF_INET, SOCK_STREAM)
-        self.HOSTNAME = "localhost"
-        self.PORT = 6000
-        self.ADDRESS = gethostbyname(self.HOSTNAME)
-
-    def send(self, command):
-        if len(command) == 0:
-            return
-        to_byte_command = command.encode(encoding='UTF-8')
-        self.socket.sendto(to_byte_command, (self.ADDRESS, self.PORT))
-
-    def receive(self):
-        message, arr = self.socket.recvfrom(4096)
-        message = message.decode("UTF-8")
-        print("message: ", message)
-        print("arr: ", arr)
-        return message
-
-
-if __name__ == "__main__":
-    player = Player0()
-    command = "(init teamKazu (goalie))"
-    player.send(command)
-    print("send:", command)
-    message = player.receive()
-    print("receive:", message)
-    print("completed")
-    while True:
-        message = player.receive()
-
-# TCP/IP による通信
+#
+#
+# # UDPによる通信
 # class Player0():
 #     def __init__(self):
 #         # UDPはSOCK_DGRAM
 #         # TCPIPはSOCK_STREAM
-#         # self.socket = socket(AF_INET, SOCK_DGRAM)
-#         self.socket = socket(AF_INET, SOCK_STREAM)
+#         self.socket = socket(AF_INET, SOCK_DGRAM)
+#         # self.socket = socket(AF_INET, SOCK_STREAM)
 #         self.HOSTNAME = "localhost"
 #         self.PORT = 6000
 #         self.ADDRESS = gethostbyname(self.HOSTNAME)
-#         self.socket.connect((self.ADDRESS, self.PORT))
 #
 #     def send(self, command):
 #         if len(command) == 0:
 #             return
 #         to_byte_command = command.encode(encoding='UTF-8')
-#         self.socket.sendall(to_byte_command)
+#         self.socket.sendto(to_byte_command, (self.ADDRESS, self.PORT))
 #
 #     def receive(self):
-#         message = self.socket.recv(1024)
+#         message, arr = self.socket.recvfrom(4096)
 #         message = message.decode("UTF-8")
+#         print("message: ", message)
+#         print("arr: ", arr)
 #         return message
-#         # print(data)
 #
 #
 # if __name__ == "__main__":
@@ -72,3 +36,39 @@ if __name__ == "__main__":
 #     print("completed")
 #     while True:
 #         message = player.receive()
+
+# TCP/IP による通信
+class Player0():
+    def __init__(self):
+        # UDPはSOCK_DGRAM
+        # TCPIPはSOCK_STREAM
+        # self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.HOSTNAME = "localhost"
+        self.PORT = 6000
+        self.ADDRESS = gethostbyname(self.HOSTNAME)
+        self.socket.connect((self.ADDRESS, self.PORT))
+
+    def send(self, command):
+        if len(command) == 0:
+            return
+        to_byte_command = command.encode(encoding='UTF-8')
+        self.socket.send(to_byte_command)
+
+    def receive(self):
+        message = self.socket.recv(1024)
+        message = message.decode("UTF-8")
+        return message
+        # print(data)
+
+
+if __name__ == "__main__":
+    player = Player0()
+    command = "(init teamKazu (goalie))"
+    player.send(command)
+    print("send:", command)
+    message = player.receive()
+    print("receive:", message)
+    print("completed")
+    while True:
+        message = player.receive()
