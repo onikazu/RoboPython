0ebbe8e2e5d4d43a3e236ca63e7f7c650076ea23
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-08-07 16:49:17 +0900

    fix

--- src/player6.py
+++ src/player6.py
@@ -10,7 +10,7 @@ class Player6(player5.Player5, threading.Thread):
 
     # 自分がボールに一番近いかどうかの判断
     def checkNearest(self, message, ballDist, ballDir):
-        teamname = "(p " + self.m_strTeamName
+        teamname = "(p \"" + self.m_strTeamName + "\""
         player = self.getObjectMessage(message, "((p")
         # print("player infomation: ", player)
         index0 = player.find(teamname, 0)
