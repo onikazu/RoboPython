020e7339eeeb2176629325763f9c8fe76cd101d3
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-7.local>
2018-08-17 13:08:27 +0900

    fix

--- src/player18.py
+++ src/player18.py
@@ -57,7 +57,7 @@ class Player18(player17.Player17, threading.Thread):
             self.m_dBallVX[t] = self.m_dX[t] - self.m_dX[pre]
             self.m_dBallVY[t] = self.m_dY[t] - self.m_dY[pre]
 
-        if len(st) > 4:
+        if len(st) >= 4:
             dist_change = self.getParam(message, "(b)", 3)
             dir_change = self.getParam(message, "(b)", 4)
             vx = dist_change
